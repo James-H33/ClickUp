36a00ac044153d19fcb0e6848c57745877f792b3
James Hall-Treworgy <llahwj33@gmail.com>
2023-07-21 08:46:34 -0400

    Fixed an issue where sort order was updating incorrectly.

--- src/app/features/views/board-view/mock-board-data.ts
+++ src/app/features/views/board-view/mock-board-data.ts
@@ -32,14 +32,14 @@ export const MockTasks: any[] = [
   { id: makeGuid(),
     statusId: MockStatuses[0].id,
     name: 'Get to work',
-    description: '',
+    description: '6AM Grind starts now',
     createdDate: '2023-06-12T20:09:00.000Z',
     position: 0
   },
   { id: makeGuid(),
     statusId: MockStatuses[0].id,
     name: 'Call ClickUp',
-    description: '',
+    description: 'Hope for offer letter',
     createdDate: '2023-06-16T18:11:00.000Z',
     position: 1
   },
@@ -47,7 +47,7 @@ export const MockTasks: any[] = [
   { id: makeGuid(),
     statusId: MockStatuses[1].id,
     name: 'Pick up groceries',
-    description: '',
+    description: 'Food is good.',
     createdDate: '2023-06-10T19:22:00.000Z',
     position: 0
   },
@@ -63,7 +63,7 @@ export const MockTasks: any[] = [
   { id: makeGuid(),
     statusId: MockStatuses[3].id,
     name: 'Fall asleep',
-    description: '',
+    description: 'Good habits, good developer, happy life.',
     createdDate: '2023-06-12T15:09:00.000Z',
     position: 0
   },
@@ -71,7 +71,7 @@ export const MockTasks: any[] = [
   { id: makeGuid(),
     statusId: MockStatuses[4].id,
     name: 'Get up',
-    description: '',
+    description: 'Coffee then code.',
     createdDate: '2023-06-12T:20:21:000Z',
     position: 0
   },
